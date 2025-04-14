d310c6fd117906d13a3805272813837d10def35c
LapizJusoh <lapizjusoh@yahoo.com>
2025-04-14 09:24:16 +0800

    Add exam mark models

--- app/Models/ExamMark.php
+++ app/Models/ExamMark.php
@@ -3,8 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class ExamMark extends Model
-{
-    //
+{   
+    protected $table = 'exam_marks';
+    protected $primaryKey = 'id';
+    protected $fillable = ['student_id','course_id','marks'];
+    use HasFactory;
 }
